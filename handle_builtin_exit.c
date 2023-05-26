869d237334ab5c05eb652414fc2bb45e4ffcf2a6
chukwucyprian <chukwucyprian262@gmail.com>
2023-05-26 17:35:51 +0000

    corrected task8

--- handle_builtin_exit.c
+++ handle_builtin_exit.c
@@ -10,20 +10,14 @@ int handle_exit_status(char *command)
 size_t exit_len = 4; /* Length of the "exit" command */
 char *arg;
 
-/* Find the first non-space character */
+ /* Find the first non-space character */
 while (*command == ' ')
 command++;
- /* Check if the command is "exit" */
-if (command[0] == 'e' && command[1] == 'x'
-		&& command[2] == 'i' && command[3] == 't' &&
-(command[exit_len] == ' ' || command[exit_len] == '\0'))
-{
-exit(0); /* Exit the shell with status 0 */
-}
 
-/* Check if the command starts with "exit " */
+/* Check if the command is "exit" */
 if (command[0] == 'e' && command[1] == 'x' && command[2] == 'i'
-		&& command[3] == 't' && command[exit_len] == ' ')
+		&& command[3] == 't' &&
+(command[exit_len] == ' ' || command[exit_len] == '\0'))
 {
 /* Extract the argument from the command */
 arg = command + exit_len + 1;
@@ -37,12 +31,17 @@ while (*arg != '\0')
 /* Check if the character is a digit */
 if (*arg < '0' || *arg > '9')
 return (0); /* Command is not valid */
+
 status = status * 10 + (*arg - '0');
 arg++;
 }
-exit(status); /* Exit the shell with the specified status */
+_exit(status); /* Exit the shell with the specified status */
 }
+else
+{
+_exit(0); /* Exit the shell with status 0 if no argument is provided */
 }
-return (0); /* Command was not the exit command */
 }
 
+return (0); /* Command was not the exit command */
+}
